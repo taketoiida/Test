5e5bf920fba765f45dbb58ea13b2cf3eef70e944
taketoiida <taketo.iida.e379@dirbato.co.jp>
2024-04-09 17:18:32 +0900

    Test0409_1718

---
+++ Test/src/calc/CalculatorTest.java
@@ -0,0 +1,22 @@
+package calc;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void testAdd(){
+    Calculator calculator = new Calculator();
+    assertEquals(5, calculator.add(2,  3));
+    }
+
+    @Test
+    void testSubtract(){
+        Calculator calculator = new Calculator();
+        assertEquals(-1,  calculator.subtract(2, 3));
+    }
+
+}
